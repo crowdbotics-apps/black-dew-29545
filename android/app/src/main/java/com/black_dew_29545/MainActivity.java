7de10405fc94fa055b5cec4c5e02a1d61e55851a
Crowdbotics <team@crowdbotics.com>
2021-08-06 02:22:43 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/black_dew_29545/MainActivity.java
@@ -0,0 +1,15 @@
+package com.black_dew_29545;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "black_dew_29545";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "black_dew_29545Identifier",
+  url: "https://black_dew_29545Identifier.botics.co",
+  api: "https://black_dew_29545Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
